87c07b1eadaeec590523cd2dc1f7c8e9d9debae2
Nikesh Bhandari <bhandarinikesh93@gmail.com>
2025-08-01 10:44:04 +0545

    Allowing HTTPs in CORS

--- Sajilotantra-web-backend/app.js
+++ Sajilotantra-web-backend/app.js
@@ -49,7 +49,7 @@ app.use(express.json({ limit: '10kb' })); // Limit body size to 10kb
 app.use(activityLogger);
 
 const corsOptions = {
-  origin: [ "http://localhost:5173"],
+  origin: [ "https://localhost:5173", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
